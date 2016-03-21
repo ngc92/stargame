1da872de420bdd5a5c0038fc75fe22e33e4d893f
Feli <fkreutz93@gmail.com>
2016-03-21 15:48:09 +0100

    überarbeitete Optinen

--- src/Options.cpp
+++ src/Options.cpp
@@ -1,83 +1,70 @@
 #include "Options.h"
 #include <iostream>
-/*#include "Game.h"
-
-#include "SoundOptions.h"
-*/
 using namespace irr;
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
 
 // Standardeinstellungen
-Options::Options():					driverType(irr::video::EDT_NULL),
-									size(irr::core::dimension2d<irr::s32>(400,300)),
-									fullscreen(false),
-									colorDepth(24)
+Options::Options():	driverType(video::EDT_NULL),
+					size(core::dimension2d<s32>(400,300)),
+					fullscreen(false),
+					colorDepth(24)
 {
 }
 
-//Funktionen zum Auslesen und Setzen
 // Driver Type
-void  Options::setDriverType(irr::video::E_DRIVER_TYPE dt){
-	if(driverType != dt)
-		mustReset = true;
+void  Options::setDriverType(video::E_DRIVER_TYPE dt)
+{
 	driverType = dt;
 }
 
-irr::video::E_DRIVER_TYPE Options::getDriverType() const{
+video::E_DRIVER_TYPE Options::getDriverType() const
+{
 	return driverType;
 }
 
 // Auflösung
-void Options::setViewportSize(irr::core::dimension2d<u32> nsize) {
-	if(nsize != size)
-		mustReset = true;
+void Options::setViewportSize(core::dimension2d<u32> nsize)
+{
 	size = nsize;
 }
 
-irr::core::dimension2d<u32> Options::getViewportSize() const {
+core::dimension2d<u32> Options::getViewportSize() const
+{
 	return size;
 }
 
 // Vollbildmodus
-void Options::setFullscreen(bool fscreen) {
-	if(fullscreen!=fscreen)
-		mustReset = true;
+void Options::setFullscreen(bool fscreen)
+{
 	fullscreen = fscreen;
 }
-bool Options::getFullscreen() const {
+
+bool Options::getFullscreen() const
+{
 	return fullscreen;
 }
 
 // Farbtiefe
-void Options::setColorDepth(irr::s32 dpt){
-	if(dpt != colorDepth)
-		mustReset = true;
+void Options::setColorDepth(irr::s32 dpt)
+{
 	colorDepth = dpt;
 }
-irr::s32 Options::getColorDepth() const{
+
+s32 Options::getColorDepth() const
+{
 	return colorDepth;
 }
 
-// -------------------------------------------------------------------------------------------------
-
-bool Options::getReset() const{
-	return mustReset;
-}
-
 // IrrlichtDevice mit den Anforderungen erstellen
-irr::IrrlichtDevice* Options::startDevice() {
-	irr::IrrlichtDevice* d = irr::createDevice(driverType, size, colorDepth, fullscreen, false, false, 0);
-	mustReset = false;
+IrrlichtDevice* Options::startDevice()
+{
+	irr::IrrlichtDevice* d = createDevice(driverType, size, colorDepth, fullscreen, false, false, 0);
 	if( d == 0 ) {
 		std::cerr << "\nCould not create device! Please check options.xml!\n";
 		std::cout << "\n\n\n\aCould not create device! Please check options.xml!\n\tType h for more information!\n\t";
 		char h;
 		std::cin >> h;
-		if(h == 'h'){
+		if(h == 'h')
+			{
 			std::cout<<	"The engine could not create a device. Maybe the settings are not correct. "
 						"Look at the values of driverType, viewportWidth, viewportHeight and colorDepth. "
 						"Probably one of these values is not correct or not supported by your system. Try to fix "
@@ -86,7 +73,8 @@ irr::IrrlichtDevice* Options::startDevice() {
 						"to quit the game\n";
 			char d;
 			std::cin >> d;
-			if(d == 'd'){
+			if(d == 'd')
+			{
 				getchar(); //ENTER abfangen
 				std::cout<<	"\nAviable driver types:\n\tSOFTWARE\t-\tVery dirty but should run on all platforms\n"
 							"\tBURNINGSVIDEO\t-\tBetter quality than SOFTWARE but slower\n"
